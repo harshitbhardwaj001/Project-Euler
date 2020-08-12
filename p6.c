2765f27aae146f1529fc08ba5428609297a9179e
harshitbhardwaj001 <68640818+harshitbhardwaj001@users.noreply.github.com>
2020-08-12 18:56:36 +0530

    Create p6.c

---
+++ p6.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    long sum;
+    int sumsqr;
+    long diff;
+    long final;
+    final = 0;
+    diff = 0;
+    sum = 0;
+    sumsqr = 0;
+
+    for(int i = 0; i <= 100; i++)
+    {
+        sum += i * i;
+    }
+    for( int j = 0; j <= 100; j++)
+    {
+        sumsqr += j;
+    }
+    final = sumsqr * sumsqr;
+    diff = final - sum;
+    printf("%ld\n", diff);
+}
