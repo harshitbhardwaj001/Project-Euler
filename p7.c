45645c11f9b9dbec51563262a072cf0e553a77e5
harshitbhardwaj001 <68640818+harshitbhardwaj001@users.noreply.github.com>
2020-08-12 18:56:09 +0530

    Update p7.c

--- p7.c
+++ p7.c
@@ -1,1 +1,21 @@
+// Completely Flawed
+// Gotta Redo.
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    long prime[10002];
+    for(int i = ; prime[i] <= prime[10001]; i++)
+    {
+        for(int j = 2; j < i; j++)
+        {
+            if(i % j != 0)
+            {
+                prime[i] = i;
+            }
+        }
+    }
+    printf("%ld\n", prime[10001] );
 }
