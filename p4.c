2beffd3d328d9a48a7f5a036329cb37f83906ad7
harshitbhardwaj001 <68640818+harshitbhardwaj001@users.noreply.github.com>
2020-08-12 18:56:29 +0530

    Create p4.c

---
+++ p4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+    int x , y;
+    int max, product, revProd, reverse;
+    max = 0;
+    for(x = 100; x < 1000; x++)
+    {
+        for(y = 100; y < 1000; y++)
+        {
+            product = x * y;
+            revProd = product;
+            reverse = 0;
+            while(revProd > 0)
+            {
+                reverse = reverse*10 + revProd % 10;
+                revProd /= 10;
+            }
+            if(reverse == product)
+            {
+                if(product > max)
+                {
+                    max = product;
+                }
+            }
+        }
+    }
+    printf("%d\n", max);
+}
