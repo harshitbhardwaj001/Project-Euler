cb8cabe42722716b0e47c6af7332806aefaf82c8
harshitbhardwaj001 <68640818+harshitbhardwaj001@users.noreply.github.com>
2020-08-12 18:56:33 +0530

    Create p5.c

---
+++ p5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<cs50.h>
+
+int main (void)
+{
+    int x, y , smallest;
+    x = y = 1;
+    smallest = 0;
+
+    while(x > 0)
+    {
+        y = 1;
+        while((x % y == 0) && (y <= 20))
+        {
+            if(y == 20)
+            {
+                smallest = x;
+                break;
+            }
+            y++;
+        }
+        x++;
+
+        if(smallest > 0)
+        {
+            break;
+        }
+    }
+    printf("%d\n", smallest);
+}
